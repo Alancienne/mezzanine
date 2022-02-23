deb150891ea753297a2cf065284f277b454020a2
aparakian <alexisparakian@gmail.com>
2022-02-23 16:08:55 +0100

    fix version in init necessary for forks

--- mezzanine/__init__.py
+++ mezzanine/__init__.py
@@ -1,1 +1,2 @@
-__version__ = "9999dev0"  # Do not edit, managed by semantic-release
+# __version__ = "9999dev0"  # Do not edit, managed by semantic-release
+__version__ = "5.1.2"  # Semantic release does not work on forks
